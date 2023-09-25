a2dd9f1c5e21b10bf6183e36613d490ff0d52e29
Emad Eldin <135942845+emadeldin12@users.noreply.github.com>
2023-09-25 15:42:59 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,121 @@
+export const shortList = [
+  {
+    id: 0,
+    image: 'https://media.licdn.com/dms/image/C4D03AQE91f5kSBXVRw/profile-displayphoto-shrink_200_200/0/1655224489292?e=1701302400&v=beta&t=_xAXWK2s86opKseOk_Ur_vtSXrzt8CnBPSrzVUujh7I',
+    name: 'Moe Hamdy',
+    title: 'Technology Director',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+];
+
+export const list = [
+  {
+   id: 5,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+   name: 'Esraa Mossad',
+   title: 'office manager',
+   quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+];
+
+export const longList = [
+  {
+    id: 1,
+    image: 'https://media.licdn.com/dms/image/C4D03AQE91f5kSBXVRw/profile-displayphoto-shrink_200_200/0/1655224489292?e=1701302400&v=beta&t=_xAXWK2s86opKseOk_Ur_vtSXrzt8CnBPSrzVUujh7I',
+    name: 'Moe Hamdy',
+    title: 'Technology Director',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+
+  {
+    id: 2,
+    image: 'https://scontent.fspx1-1.fna.fbcdn.net/v/t39.30808-6/327020452_578044130895528_7150709405731278504_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=1b51e3&_nc_ohc=OGXnGH3tj4gAX9irJIc&_nc_ht=scontent.fspx1-1.fna&oh=00_AfCT3-eUuLphPu-pUoAb0X34CNps9vJ0TnJnFt9hpEzqBg&oe=6516A30E',
+    name: 'Emad Eldin',
+    title: 'Assistant Web Developer',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 3,
+    image: 'https://scontent.cdninstagram.com/v/t51.2885-19/382128414_996869608244322_1400131984715801708_n.jpg?stp=dst-jpg_s80x80&_nc_cat=100&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=qlOwVZTDv3QAX95BSDF&_nc_ht=scontent.cdninstagram.com&oh=00_AfCgBRVD4v0h_aURvuqBzT2tkmWumf4NEdkxXPgrRSlz2w&oe=651661F4',
+    name: 'Esraa Masaad',
+    title: 'Assistant Web Developer',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache',
+  },
+  {
+    id: 4,
+    image: 'https://scontent.fspx1-1.fna.fbcdn.net/v/t39.30808-6/290366235_1877251019137715_8152059084364071853_n.jpg?stp=dst-jpg_p526x296&_nc_cat=103&ccb=1-7&_nc_sid=5614bc&_nc_ohc=6DDinC6l1egAX9yGmsK&_nc_ht=scontent.fspx1-1.fna&oh=00_AfBDBHIx_ylFHf3osL5UQU9VztSf0mo5-drL9D85DLGXjg&oe=6515F808',
+    name: 'Ibrahim Ramadan',
+    title: 'Business Systems and IT Administrator',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 5,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+  {
+    id: 6,
+    image: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'maria ferguson',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 7,
+    image: 'https://www.course-api.com/images/people/person-4.jpeg',
+    name: 'john doe',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 8,
+    image: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 9,
+    image: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+];
